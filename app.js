b8525861fd153cef16d83355f05a5373a5a78bde
parlom <shalom.iyeze@gmail.com>
2022-02-22 16:38:16 +0200

    first part of menu peoject covered well

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-const menu = [
-  {
+const menu = [{
     id: 1,
     title: "buttermilk pancakes",
     category: "breakfast",
@@ -72,3 +71,30 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+const sectionCenter = document.querySelector(`.section-center`);
+window.addEventListener('DOMContentLoaded', function () {
+  displayMenuItems(menu)
+});
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item)
+    return `
+      <article class="menu-item">
+        <img src=${item.img} alt=${item.title} class="photo">
+        <div class="item-info">
+          <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">${item.price}</h4>
+          </header>
+          <p class="item-text">
+          ${item.desc}
+          </p>
+        </div>
+      </article>`
+  });
+  displayMenu = displayMenu.join('')
+  console.log(displayMenu);
+  sectionCenter.innerHTML = displayMenu;
+}
